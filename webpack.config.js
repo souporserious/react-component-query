c469155486f71aa8a7139437ec1477dbf5bdb57f
Travis Arnold <ftntravis@gmail.com>
2016-12-02 15:03:27 -0800

    inject webpack dev server, add bundle to example folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,40 @@
 var path = require('path');
-var nodeModulesDir = path.resolve(__dirname, 'node_modules');
+var webpack = require('webpack');
+var TARGET = process.env.TARGET || null;
 
-module.exports = {
+var config = {
   entry: {
-    index: ['webpack/hot/dev-server', './example/index.jsx']
+    index: [
+      'webpack/hot/dev-server',
+      path.join(__dirname, 'example/index.jsx')
+    ]
   },
   output: {
-    path: './example',
+    path: path.join(__dirname, 'example'),
     filename: 'bundle.js'
   },
   module: {
     loaders: [
-      { test: /\.(js|jsx)/, loader: 'babel-loader' },
+      { test: /\.(js|jsx)/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.scss$/, loader: 'style!css!postcss!sass?sourceMap' }
     ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+  plugins: [],
   devServer: {
     contentBase: './example',
+    inline: true
   }
 };
+
+if (TARGET === 'minify') {
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  }));
+}
+
+module.exports = config;
